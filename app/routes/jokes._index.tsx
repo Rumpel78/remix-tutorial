f36de31fc3d3142bcc1440ac9e359e1370898e13
Thomas Rzipa <thomas.rzipa@fusonic.net>
2023-09-12 07:32:05 +0200

    add login and anything

--- app/routes/jokes._index.tsx
+++ app/routes/jokes._index.tsx
@@ -1,6 +1,12 @@
-import { Link, useLoaderData } from "@remix-run/react";
-import { db } from "../utils/db.server";
 import { json } from "@remix-run/node";
+import {
+  isRouteErrorResponse,
+  Link,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
 
 export const loader = async () => {
   const count = await db.joke.count();
@@ -9,17 +15,37 @@ export const loader = async () => {
     skip: randomRowNumber,
     take: 1,
   });
+  if (!randomJoke) {
+    throw new Response("No random joke found", {
+      status: 404,
+    });
+  }
   return json({ randomJoke });
 };
 
 export default function JokesIndexRoute() {
-  const { randomJoke } = useLoaderData<typeof loader>();
+  const data = useLoaderData<typeof loader>();
 
   return (
     <div>
       <p>Here's a random joke:</p>
-      <p>{randomJoke.content}</p>
-      <Link to={randomJoke.id}>"{randomJoke.name}" Permalink</Link>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
     </div>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return (
+      <div className="error-container">
+        <p>There are no jokes to display.</p>
+        <Link to="new">Add your own</Link>
+      </div>
+    );
+  }
+
+  return <div className="error-container">I did a whoopsies.</div>;
 }
